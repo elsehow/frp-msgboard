ac2b8f63bd88f00392b5be39c34fe69ed22e144e
elsehow <ffff@ffffs-MacBook-Air.local>
2015-03-15 00:13:09 -0700

    added spec to PostBox.js

--- src/browser/PostBox.js
+++ src/browser/PostBox.js
@@ -6,68 +6,74 @@ var _ = require('lodash')
 , utils = require('../common/Utils')
 , postBoxTemplates = require('./templates/PostBoxTemplate')
 
+// SPECS:
+// while typing, should embed URLs the user types in
+// but, only one URL at a time
+// an X button should remove the url
+// and, once an url is removed, it shouldn't be added again
+
+// TODO
+// get-urls module isnt very good (doesnt trim off either side of the url)
+// urls should return embed data (even if its fake) to practice ajax streams
+
 exports.setup = function() {
 
-	// a bacon model representing the current embed
-	var currentEmbed = baconModel.Model(undefined)
-	var postBoxModel = {
-		embeddedURLs: []
-		// this is a bacon model
-		, currentEmbed: currentEmbed 
-		// functions	
-		, setCurrentEmbed: function (value) {  currentEmbed.set(value) }
-		, appendToEmbeddedURLs: function (url) { postBoxModel.embeddedURLs.push(url) }
-		, clearCurrentEmbed: function () { currentEmbed.set(undefined) }
-		, getFirstNewURL: function (text) { return embedLinks.getFirstNewURL(text, postBoxModel.embeddedURLs) }
-		, embedExists: function () { return utils.truthy(currentEmbed.get())  }
-		, setEmbedIfNoneExists: function (url) { if (!postBoxModel.embedExists()) postBoxModel.setCurrentEmbed(url) }
-	} 
+  // a bacon model representing the current embed
+  var currentEmbed = baconModel.Model(undefined)
+  var postBoxModel = {
+    embeddedURLs: []
+    // this is a bacon model
+    , currentEmbed: currentEmbed 
+    // functions  
+    , setCurrentEmbed: function (value) {  currentEmbed.set(value) }
+    , appendToEmbeddedURLs: function (url) { postBoxModel.embeddedURLs.push(url) }
+    , clearCurrentEmbed: function () { currentEmbed.set(undefined) }
+    , getFirstNewURL: function (text) { return embedLinks.getFirstNewURL(text, postBoxModel.embeddedURLs) }
+    , embedExists: function () { return utils.truthy(currentEmbed.get())  }
+    , setEmbedIfNoneExists: function (url) { if (!postBoxModel.embedExists()) postBoxModel.setCurrentEmbed(url) }
+  } 
 
-	// get the value of #postInput on keydown
-	var postInputProperty = bjq.textFieldValue($('#postBox input'))
-		// limit the rate of updates
-		.debounce(150)
-		// skip items in the stream with the same text
-		.skipDuplicates()
+  // get the value of #postInput on keydown
+  var postInputProperty = bjq.textFieldValue($('#postBox input'))
+    // limit the rate of updates
+    .debounce(150)
+    // skip items in the stream with the same text
+    .skipDuplicates()
 
-	// disable the post button unless postInputProperty is nonempty
- 	var postButtonDisabled = postInputProperty.map(utils.nonEmpty)
-	postButtonDisabled.assign(utils.setEnabled, $('#postButton'))
+  // disable the post button unless postInputProperty is nonempty
+  var postButtonDisabled = postInputProperty.map(utils.nonEmpty)
+  postButtonDisabled.assign(utils.setEnabled, $('#postButton'))
 
-	// pressing the removeEmbed button removes postBoxModel.currentEmbed
-	var clearEmbedStream = $("#removePostEmbed")
-		.asEventStream('click')
-		.onValue(postBoxModel.clearCurrentEmbed)
+  // pressing the removeEmbed button removes postBoxModel.currentEmbed
+  var clearEmbedStream = $("#removePostEmbed")
+    .asEventStream('click')
+    .onValue(postBoxModel.clearCurrentEmbed)
 
-	// the first URL that appears in the post
-	// that we haven't already embedded 
-	var firstNewURLInPost = postInputProperty
-		// skip items in the stream that are the same
-		.skipDuplicates()
-		.map(postBoxModel.getFirstNewURL) 
-		.filter(utils.truthy)
-		.toProperty()
+  // the first URL that appears in the post
+  // that we haven't already embedded 
+  var firstNewURLInPost = postInputProperty
+    // skip items in the stream that are the same
+    .skipDuplicates()
+    .map(postBoxModel.getFirstNewURL) 
+    .filter(utils.truthy)
+    .toProperty()
 
-	// TODO we should actually get some embed data w ajax()
-	firstNewURLInPost
-		.onValue(postBoxModel.setEmbedIfNoneExists)
+  // TODO we should actually get some embed data w ajax()
+  firstNewURLInPost
+    .onValue(postBoxModel.setEmbedIfNoneExists)
 
-	// ok! we got an embed
-	currentEmbed
-		// we can now set it to our previously seen embeds
-		.onValue(function (url) {
-			// if the embed is falsey, hide #embedContainer
-			if (!url) { 
-				utils.setVisibility($('#embedContainer'), false) 
-			} else {
-			// if the embed is real, show #embedContainer
-				utils.setVisibility($('#embedContainer'), true) 
-			// add it to the list of embeds we've embedded
-				postBoxModel.appendToEmbeddedURLs(url)
-			// and set #postEmbed to the embed content
-				$("#postEmbed").html(
-					postBoxTemplates.embedTemplate(url)
-				)
-			}
-		})
+  // ok! we got an embed
+  currentEmbed
+    .onValue(function (url) {
+      // if the embed is real, show #embedContainer
+      // if the embed is falsey, hide #embedContainer
+        utils.setVisibility($('#embedContainer'), utils.truthy(url)) 
+      // add it to the list of embeds we've embedded
+        postBoxModel.appendToEmbeddedURLs(url)
+      // and set #postEmbed to the embed content
+        $("#postEmbed").html(
+          postBoxTemplates.embedTemplate(url)
+        )
+      }
+    })
 }
